1a4349a9c2f52d6fc58a63705b5e5f9fdbb99fdd
hyfyooie <hyfyooie@outlook.com>
2020-04-08 21:54:11 +0700

    New project; old project commented out

--- script.js
+++ script.js
@@ -12,16 +12,63 @@
 // let errorCase = promise3.catch(e => {
 //   console.log('There has been a problem with your fetch operation: ' + e.message);
 // })
+//
+//
+// fetch('boo1.png')
+// .then(response => response.blob())
+// .then(myBlob => {
+//   let objectURL = URL.createObjectURL(myBlob);
+//   let image = document.createElement('img');
+//   image.src = objectURL;
+//   document.body.appendChild(image);
+// })
+// .catch(e => {
+//   console.log('There has been a problem with your fetch operation: ' + e.message);
+// });
 
+// let a = fetch(url1);
+// let b = fetch(url2);
+// let c = fetch(url3);
 
-fetch('boo1.png')
-.then(response => response.blob())
-.then(myBlob => {
-  let objectURL = URL.createObjectURL(myBlob);
-  let image = document.createElement('img');
-  image.src = objectURL;
-  document.body.appendChild(image);
-})
-.catch(e => {
-  console.log('There has been a problem with your fetch operation: ' + e.message);
+// Promise.all([a, b, c]).then(values => {
+//   // ...
+// });
+
+function fetchAndDecode(url, type) {
+  return fetch(url).then(response => {
+    if (type === 'blob') {
+      return response.blob();
+    } else if (type === 'text') {
+      return response.text();
+    }
+  })
+  .catch(e => {
+    console.log('There has been a problem with your fetch operation: ' + e.message);
+  })
+}
+
+let booOriginal = fetchAndDecode('boo.png', 'blob');
+let booPixel = fetchAndDecode('boo-pixel.png', 'blob');
+let text = fetchAndDecode('news.txt', 'text');
+
+Promise.all([booOriginal, booPixel, text]).then(values => {
+  console.log(values);
+  // Store each value returned from the promises in separate variables; create object URLs from the blobs
+  let objectURL1 = URL.createObjectURL(values[0]);
+  let objectURL2 = URL.createObjectURL(values[1]);
+  let decText = values[2];
+
+  // Display the images in <img> elements
+  let image1 = document.createElement('img');
+  let image2 = document.createElement('img');
+  image1.src = objectURL1;
+  image2.src = objectURL2;
+  document.body.appendChild(image1);
+  document.body.appendChild(image2);
+
+  // Display the text in a paragraph
+  let para = document.createElement('p');
+  para.textContent = descText;
+  document.body.appendChild(para);
+
 });
